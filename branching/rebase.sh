261b4aff529db1307f90e3c2d5b433be15edb7c3
andrey.isaev <maga14881337@icloud.com>
2022-06-14 21:40:41 +0300

    I'm trying to rebase, I think i've got this

--- branching/rebase.sh
+++ branching/rebase.sh
@@ -1,9 +1,11 @@
+cat rebase.sh
 #!/bin/bash
 # display command line options
 
 count=1
 for param in "$@"; do
-    echo "\$@ Parameter #$count = $param"
+
+    echo "Parameter: $param"
     count=$(( $count + 1 ))
 done
 
