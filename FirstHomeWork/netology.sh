cde8adbf83dea0d852b47c4c401cc322f3f3e464
andrey.isaev <maga14881337@icloud.com>
2022-06-15 21:57:12 +0300

    added my 1st homework to my repo

---
+++ FirstHomeWork/netology.sh
@@ -0,0 +1,4 @@
+STUDENT_NAME="Andrey Isaev"
+
+echo "my name in ${STUDENT_NAME}"
+echo "this is first try to use pycharm for git" >> pycharm.md
---
+++ FirstHomeWork/netology.tf
@@ -0,0 +1,11 @@
+#Andrey Isaev
+provider "aws" {
+  region  = "us-west-2"
+  version = "~> 2.18"
+}
+
+resource "aws_cloudwatch_log_group" "lambda" {
+  name = "/aws/lambda/Andrey_Isaev"
+
+  retention_in_days = 1
+}
